1bb3798c6fdf923e3b88242bf86ca67bb2cee450
Martin Madej <martinpes2009@gmail.com>
2017-05-10 13:05:16 +0200

    make choicer and stakes for compare.

--- main/main.py
+++ main/main.py
@@ -13,16 +13,47 @@ def load_jsons(list_of_backends):
     return backends
 
 
+def print_stake(STS_STAKE, BWIN_STAKE):
+    print("STS stake {}".format(STS_STAKE))
+    print("BWIN stake {}".format(BWIN_STAKE))
+
+
 def search_team_name_in_backends(backends):
-    s = 'Schalke'
-    for i in backends:
-        for backend in backends.get(i, 'do not found backend'):
-            if s in backend.get('meta').get('away'):
-                print(s)
-                print(backend)
-                print(str(i))
+    STS_STAKE = 1.0
+    BWIN_STAKE = 1.0
+    x = True
+    while x == True:
+        first_team_name = input("name of the 1st team")
+        second_team_name = input("name of the 2th team")
+        for i in backends:
+            for backend in backends.get(i, 'backend not found'):
+                if first_team_name in backend.get('meta').get('home') and second_team_name in backend.get('meta').get(
+                        'away'):
+                    print("buchmaker {}".format(i))
+                    print("|  1  |  X  |  2 |")
+                    print("|{} |{} |{}|".format(backend.get('1').get('odds'), backend.get('X').get('odds'),
+                                                backend.get('2').get('odds')))
+                    if i == 'sts':
+                        sts = backend
+                    else:
+                        bwin = backend
+
+        choice = input("wybierz 1, X, 2")
+        sts_odds = float(sts.get(choice).get('odds'))
+        bwin_odds = float(bwin.get(choice).get('odds'))
+        STS_STAKE *= sts_odds
+        BWIN_STAKE *= bwin_odds
+        z = input("True or False?")
+        if z != "True":
+            x = False
+        print_stake(STS_STAKE, BWIN_STAKE)
 
 
 if __name__ == '__main__':
     backends = load_jsons(NAMES_BACKENDS)
     search_team_name_in_backends(backends)
+
+    ################################################
+    #   zoptymalizować kod oraz #
+    #                                              #
+    ################################################
